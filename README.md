c472c3d75efab3151bbdee29966a9446f212a482
yashdeep01 <ydsd97@gmail.com>
2018-07-28 02:15:23 +0530

    Updated dependencies in README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# India GeoCoder
+# Welcome to India GeoCoder!
 **India GoeCoder** is a plugin on _QGIS 3_ to help users geocode health data from NRHM website (https://nrhm-mis.nic.in/hmisreports/frmstandard_reports.aspx) and later visualise the same by displaying data on a map and exporting the map in the form of an image.
 
 This plugin is developed specifically for Indian health data collected by Ministry of Health & Family Welfare, Government of India under the digital initiative of National Health Mission. The data is released under **Health Management Information System** and is openly available at NRHM (National Rural Health Mission) website: https://nrhm-mis.nic.in/hmisreports/frmstandard_reports.aspx. This data comprises of around *200* health indicators collected across over *5,000* sub districts.
@@ -31,3 +31,11 @@ If you received the plugin as a zip file, instead of clicking **Installed** tab 
 Spatial data for geocoding and visualisation is provided in the repository as **All.gpkg**. This is a district level geopackage file of India. Non-spatial data should be downloaded from **Performance of Key HMIS Indicators(upto District Level)** section of the NRHM website. This directory should be given as input in the first dialog while ***Loading and Joining Data***. A folder containing sample health data for district is given in the repository for reference and testing purposes. User may use *this* as the input directory discussed above.
 
 ![Dialog](https://github.com/yashdeep01/India-GeoCoder/blob/master/Screenshots/Input_directory.png)
+
+
+## Dependencies
+India GeoCoder relies on the following modules. User *must* have them installed before running the plugin.
+
+* pandas
+* BeautifulSoup
+* csv
--- india_geocoder_dialog_base.py
+++ india_geocoder_dialog_base.py
@@ -11,23 +11,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_IndiaGeocoderDialogBase(object):
     def setupUi(self, IndiaGeocoderDialogBase):
         IndiaGeocoderDialogBase.setObjectName("IndiaGeocoderDialogBase")
-        IndiaGeocoderDialogBase.resize(539, 274)
+        IndiaGeocoderDialogBase.resize(580, 269)
         self.button_box = QtWidgets.QDialogButtonBox(IndiaGeocoderDialogBase)
-        self.button_box.setGeometry(QtCore.QRect(170, 220, 341, 32))
+        self.button_box.setGeometry(QtCore.QRect(210, 220, 341, 32))
         self.button_box.setOrientation(QtCore.Qt.Horizontal)
         self.button_box.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.button_box.setObjectName("button_box")
         self.toolButton = QtWidgets.QToolButton(IndiaGeocoderDialogBase)
-        self.toolButton.setGeometry(QtCore.QRect(480, 160, 26, 22))
+        self.toolButton.setGeometry(QtCore.QRect(520, 160, 26, 22))
         self.toolButton.setObjectName("toolButton")
         self.lineEdit = QtWidgets.QLineEdit(IndiaGeocoderDialogBase)
-        self.lineEdit.setGeometry(QtCore.QRect(150, 160, 321, 21))
+        self.lineEdit.setGeometry(QtCore.QRect(150, 160, 351, 21))
         self.lineEdit.setObjectName("lineEdit")
         self.label = QtWidgets.QLabel(IndiaGeocoderDialogBase)
         self.label.setGeometry(QtCore.QRect(30, 160, 111, 21))
         self.label.setObjectName("label")
         self.textBrowser = QtWidgets.QTextBrowser(IndiaGeocoderDialogBase)
-        self.textBrowser.setGeometry(QtCore.QRect(30, 30, 481, 101))
+        self.textBrowser.setGeometry(QtCore.QRect(30, 30, 521, 101))
         self.textBrowser.setObjectName("textBrowser")
 
         self.retranslateUi(IndiaGeocoderDialogBase)
@@ -44,5 +44,5 @@ class Ui_IndiaGeocoderDialogBase(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'.SF NS Text\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#007000;\">This plugin requires HMIS data of various health indicators which can be obtained from NRHM website. Visit </span><span style=\" font-weight:600; color:#007000;\">https://nrhm-mis.nic.in/hmisreports/frmstandard_reports.aspx</span><span style=\" color:#007000;\">, go to </span><span style=\" font-family:\'Verdana,Helvetica,Arial,sans-serif\'; font-style:italic; color:#007000; background-color:#ffffff;\">Performance of Key HMIS Indicators Across Sub District</span><span style=\" font-family:\'Verdana,Helvetica,Arial,sans-serif\'; color:#007000; background-color:#ffffff;\"> and download suitable directory. Enter local path of the downloaded directory below.</span></p></body></html>"))
+"<p align=\"center\" style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#007000;\">This plugin requires HMIS data which can be obtained from NRHM website. Visit </span><span style=\" font-weight:600; color:#007000;\">https://nrhm-mis.nic.in/hmisreports/frmstandard_reports.aspx</span><span style=\" color:#007000;\">, go to </span><span style=\" font-family:\'Verdana,Helvetica,Arial,sans-serif\'; font-style:italic; color:#007000; background-color:#ffffff;\">Performance of Key HMIS Indicators(upto District Level)</span><span style=\" font-family:\'Verdana,Helvetica,Arial,sans-serif\'; color:#007000; background-color:#ffffff;\"> and download suitable directory. Enter the local path of the downloaded directory below.</span></p></body></html>"))
 
